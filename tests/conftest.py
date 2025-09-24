c136778b5d5d50a2a5ce1aee5f2a64b387d3531f
tbuell124 <tybuell@gmail.com>
2025-09-24 09:27:08 -0400

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Ensure the project root is on sys.path so that the "sprinkler" module can
-# be imported reliably when tests are executed from within the tests folder.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
